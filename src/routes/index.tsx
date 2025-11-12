403462a9cb4cfc621c0702249a69aa83ee95e2ec
Ameksa khadija <ameksakhadija026@gmail.com>
2025-11-12 14:33:27 +0100

    stricture de projet

--- src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Login from "../pages/public/Login";
-import Dashboard from "../pages/protected/Dashboard";
-
-export const routes = [
-  { path: "/login", element: <Login /> },
-  { path: "/dashboard", element: <Dashboard /> },
-  // ajouter routes protégées et rôles ici
-];
